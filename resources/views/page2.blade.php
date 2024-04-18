33dedf34fe368f5ed79d42930aed0c0c3cb4b910
zeros <luca.perini.illustrations@gmail.com>
2024-04-18 19:46:16 +0200

    Esercizio completato

---
+++ resources/views/page2.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Chi siamo</h1>
+    <ul>
+        <li><a href="/">Welcome</a></li>
+        <li><a href="/page1">Chi eravamo</a></li>
+        <li><a href="/page3">Chi saremo</a></li>
+    </ul>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/page1', function () {
+    return view('page1');
+});
+Route::get('/page2', function () {
+    return view('page2');
+});
+Route::get('/page3', function () {
+    return view('page3');
+});
+
+
